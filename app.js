bba4230d479b715739526f4e81e3b217e84f5ed2
joellaprade <145079600+joellaprade@users.noreply.github.com>
2024-01-08 15:20:02 -0600

    nvm

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+const dbURI = 'mongodb+srv://jlaprade:today2428M@nodetuts.3rfiajp.mongodb.net/events?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+        console.log('listening')
+    })
+    .catch((err) => console.log(err))
+
+app.listen(port)
+app.use(express.urlencoded({ extended: true }))
+// app.use(express.static('public'))
+app.use(express.json())
+
+
+
+app.get('/test', async (req, res) => {
+    console.log('ran test')
+    res.send(JSON.stringify({message: 'Working fucker'}));
+})
