1e406fff987d3d09c61c847c862213af02f76638
joellaprade <145079600+joellaprade@users.noreply.github.com>
2024-01-08 18:30:52 -0600

    r

--- app.js
+++ app.js
@@ -22,9 +22,7 @@ app.use(express.json())
 app.get('/productos', async (req, res) => {
     await getTracker();
     await Tracker.findByIdAndUpdate('659c855c436a8377c2f9bf9e', {
-        inicio: tracker.inicio,
         productos: tracker.productos + 1,
-        proyectos: tracker.proyectos
     })
     res.sendFile('/home/jlaprade/public_html/products/products.html')
 })
