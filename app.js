8fadafea625f438d20f3d195913c289f2d4b95b8
joellaprade <145079600+joellaprade@users.noreply.github.com>
2024-01-08 18:20:05 -0600

    f

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 8000;
 const Tracker = require('./models/tracker');
-var tracker = Tracker.findById('659c855c436a8377c2f9bf9e')
+var tracker = await tracker.findById('659c855c436a8377c2f9bf9e')
 
 const dbURI = 'mongodb+srv://joellaprade:kZvCIqWpNNBNUn2K@jlapradecluster.xdnaioy.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
