95c0435fe3c8f18d4cad056c667d6059a72b86d2
joellaprade <145079600+joellaprade@users.noreply.github.com>
2024-01-08 19:40:10 -0600

    getTime

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 8000;
 const Tracker = require('./models/tracker');
+var date = new Date();
 var tracker;
 
 const dbURI = 'mongodb+srv://joellaprade:kZvCIqWpNNBNUn2K@jlapradecluster.xdnaioy.mongodb.net/?retryWrites=true&w=majority'
@@ -24,6 +25,7 @@ app.get('/', async (req, res) => {
     await Tracker.findByIdAndUpdate('659c855c436a8377c2f9bf9e', {
         inicio: tracker.inicio + 1,
     })
+    console.log('Logged to / at: ' + getTime())
     res.sendFile('/home/jlaprade/public_html/inicio.html')
 })
 
@@ -32,6 +34,7 @@ app.get('/productos', async (req, res) => {
     await Tracker.findByIdAndUpdate('659c855c436a8377c2f9bf9e', {
         productos: tracker.productos + 1,
     })
+    console.log('Logged to productos at: ' + getTime())
     res.sendFile('/home/jlaprade/public_html/products/products.html')
 })
 
@@ -40,6 +43,7 @@ app.get('/proyectos', async (req, res) => {
     await Tracker.findByIdAndUpdate('659c855c436a8377c2f9bf9e', {
         proyectos: tracker.proyectos + 1,
     })
+    console.log('Logged to proyectos at: ' + getTime())
     res.sendFile('/home/jlaprade/public_html/proyects/proyects.html')
 })
 
@@ -50,3 +54,6 @@ const getTracker = async () => {
     tracker = await Tracker.findById('659c855c436a8377c2f9bf9e')
 } 
 
+const getTime = () => {
+    return `${date.getMonth()} ${date.getDay()} ${date.getHours()} ${date.getMinutes()} ${date.getSeconds()}`
+}
